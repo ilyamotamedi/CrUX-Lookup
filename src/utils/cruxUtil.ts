b65d8abf5e309bd51f908a7b168a03d8629fa0c5
Ilya Motamedi <ilya.motamedi@gmail.com>
2022-03-29 16:48:52 -0400

    removed site existence check

--- src/utils/cruxUtil.ts
+++ src/utils/cruxUtil.ts
@@ -3,7 +3,7 @@ import { ensureConfig } from './configUtil.js';
 import fetch from 'node-fetch';
 import { createQueryRecord, SuccessResponse } from 'crux-api';
 
-import { validateUrl, validateWebsite } from './siteUtil.js';
+import validateUrl from './siteUtil.js';
 
 const newQueryRecord = () => {
   ensureConfig();
@@ -14,12 +14,9 @@ const singleLookup = async (url: string, origin?: boolean): Promise<SuccessRespo
   const queryRecord = newQueryRecord();
   if (!validateUrl(url)) {
     throw new Error(`Invalid URL: ${url}`);
-  } else if (!(await validateWebsite(url))) {
-    throw new Error(`Site doesn't exist: ${url}`);
-  } else {
-    const paramsObj = origin ? { origin: url } : { url };
-    return queryRecord(paramsObj);
   }
+  const paramsObj = origin ? { origin: url } : { url };
+  return queryRecord(paramsObj);
 };
 
 interface SimplifiedCruxHistogram {
